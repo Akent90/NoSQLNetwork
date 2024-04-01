f3ad4b1cf657961d8863903f706c9d03ccebd444
akent <alexanderkent099@gmail.com>
2024-04-01 13:20:52 -0400

    Add controllers for adding and removing reactions from thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -63,3 +63,35 @@ exports.deleteThought = async (req, res) => {
         res.status(500).json({ message: 'Error deleting this thought', error });
     }
 };
+
+exports.addReaction = async (req, res) => {
+    try {
+        const thought = await Thought.findByIdAndUpdate(
+            req.params.thoughtId,
+            { $push: { reactions: req.body } },
+            { new: true, runValidators: true }
+        );
+        if (!thought) {
+            return res.status(404).json({ message: 'No thought was found with this ID to add a reaction!' });
+        }
+        res.json(thought);
+    } catch (error) {
+        res.status(500).json({ message: 'Error adding reaction', error });
+    }
+};
+
+exports.removeReaction = async (req, res) => {
+    try {
+        const thought = await Thought.findByIdAndUpdate(
+            req.params.thoughtId,
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { new: true }
+        );
+        if (!thought) {
+            return res.status(404).json({ message: 'No thought was found with this ID to remove a reaction!' });
+        }
+        res.json(thought);
+    } catch (error) {
+        res.status(500).json({ message: 'Error removing reaction!', error });
+    }
+};
