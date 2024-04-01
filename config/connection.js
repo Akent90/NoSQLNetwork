795a90764edde287f1047abe7e3613a9843c50ae
akent <alexanderkent099@gmail.com>
2024-04-01 15:04:27 -0400

    Adding gitignore to root

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const { connect, connection } = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const mongoURI = process.env.MONGODB_URI;
+
+const options = {
+    useNewURLParser: true, 
+    useUnifiedTopology: true,
+    useFindAndModify: false, 
+    useCreateIndex: true,
+};
+
+connect(mongoURI, options)
+    .then(() => console.log('MongoDB connection established'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = connection;
