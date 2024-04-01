7bebd8ae72a11904efed6f1c632d9ec5b46eb10d
akent <alexanderkent099@gmail.com>
2024-04-01 12:36:28 -0400

    Implement thought schema with thoughtText, createdAt, username, and reactions

--- models/User.js
+++ models/User.js
@@ -31,3 +31,11 @@ const userSchema = new Schema({
     },
     id: false,
 });
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
