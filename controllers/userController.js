e6be55c191b9cdea1caa2459ce2c2bf7f2bf6d5e
akent <alexanderkent099@gmail.com>
2024-04-01 14:02:03 -0400

    Add controllers for updating and deleting users, and adding/removing friends

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,3 +34,63 @@ exports.createUser = async (req, res) => {
         res.status(500).json(err);
     }
 };
+
+exports.updateUser = async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!updatedUser) {
+            res.status(404).json({ message: 'No user was found with this ID' });
+            return;
+        }
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+exports.deleteUser = async (req, res) => {
+    try {
+        const userToDelete = await User.findByIdAndDelete(req.params.id);
+        if (!userToDelete) {
+            res.status(404).json({ message: 'No user was found with this ID' });
+            return;
+        }
+        res.json({ message: 'User was deleted successfully' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+exports.addFriend = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.userId,
+            { $addToSet: { friends: req.params.friendId } },
+            { new: true }
+        );
+        if(!user) {
+            res.status(404).json({ message: 'No user was found with this ID' });
+            return;
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+exports.removeFriend = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(
+            req.params.userId,
+            { $pull: { friends: req.params.friendId } },
+            { new: true }
+        );
+        if (!user) {
+            res.status(404).json({ message: 'No user was found with this ID' });
+            return;
+        }
+        res.json(user);
+    } catch (err) {
+        res.status(500).jason(err);
+    }
+};
