512b21651b181999701e9393882eca2a9690f6eb
akent <alexanderkent099@gmail.com>
2024-04-01 13:42:48 -0400

    Implement routes for CRUD operations on users and adding/removing friends

---
+++ routes/userRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../controllers/userController');
+
+const router = express.Router();
+
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router.route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
+
+module.exports = router;
