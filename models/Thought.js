d949469de11c7ce6176c94a1dba690eac9c27309
akent <alexanderkent099@gmail.com>
2024-04-02 12:10:27 -0400

    Fix default ObjectId generation for reactionId in reactionSchema

--- models/Thought.js
+++ models/Thought.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const reactionSchema = new Schema({
     reactionId: {
         type: Schema.Types.ObjectId,
-        default: () => new Schema.Types.ObjectId(),
+        default: () => new mongoose.Types.ObjectId(),
     },
     reactionBody: {
         type: String,
